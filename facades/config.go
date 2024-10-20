d69fd64181f477fb3938370e5d9c9759f847bead
Styg <kioravuser@gmail.com>
2024-10-20 19:13:14 +0300

    Add facade for config.

---
+++ facades/config.go
@@ -0,0 +1,17 @@
+package facades
+
+import (
+	"github.com/stygian91/veggies/config"
+	b "github.com/stygian91/veggies/config/base"
+)
+
+type conf struct{}
+
+func Config() conf {
+  return conf{}
+}
+
+func (this conf) App() b.App {
+  appConfig, _ := config.GetGroup[b.App]("app")
+  return appConfig
+}
