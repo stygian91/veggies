c403679889efe2c026d3b6d264216c0c9ba432eb
Styg <kioravuser@gmail.com>
2024-11-23 13:29:38 +0200

    Make log middleware logger instance configurable.

--- router/middleware/logmiddleware.go
+++ router/middleware/logmiddleware.go
@@ -10,6 +10,8 @@ var logCallback LogFunction = func(r *http.Request, status int) string {
 	return fmt.Sprintf("method: %s, uri: %s, status: %d, addr: %s", r.Method, r.RequestURI, status, r.RemoteAddr)
 }
 
+var logger *log.Logger = log.Default()
+
 type LogFunction func(r *http.Request, status int) string
 
 type StatusResponseWriter struct {
@@ -32,19 +34,21 @@ func (this *StatusResponseWriter) WriteHeader(code int) {
 	this.ResponseWriter.WriteHeader(code)
 }
 
-// TODO:
-// make logger that's used configurable, instead of using default logger
 var LogMiddleware Middleware = Middleware{
 	Name: "log",
 	Handler: func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			writer := StatusResponseWriter{ResponseWriter: w}
 			next.ServeHTTP(&writer, r)
-			log.Println(logCallback(r, writer.status))
+			logger.Println(logCallback(r, writer.status))
 		})
 	},
 }
 
 func SetLogCallback(cb LogFunction) {
 	logCallback = cb
+}
+
+func SetLogger(l *log.Logger) {
+	logger = l
 }
