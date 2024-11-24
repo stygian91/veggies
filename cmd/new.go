e0fe7a1965088b4ccaa593522926f26e9bfd613b
Styg <kioravuser@gmail.com>
2024-11-24 12:33:11 +0200

    Embed the whole template directory instead of separate files.

--- cmd/new.go
+++ cmd/new.go
@@ -2,32 +2,23 @@ package cmd
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
 	"os/exec"
-	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/stygian91/veggies/internal/templates"
 )
 
-type fileEntry struct{ Path, Content string }
+type rename struct{ Src, Dest string }
 
-var mapping []fileEntry = []fileEntry{
-	{Path: "/main.go", Content: templates.Main},
-	{Path: "/go.mod", Content: templates.Gomod},
-	{Path: "/sqlc.yaml", Content: templates.Sqlc},
-	{Path: "/query.sql", Content: templates.Query},
-	{Path: "/schema.sql", Content: templates.Schema},
-	{Path: "/.env", Content: templates.EnvExample},
-	{Path: "/handlers/greet.go", Content: templates.Greet},
-	{Path: "/routes/routes.go", Content: templates.Routes},
-}
-var subdirs []string = []string{
-	"/handlers",
-	"/routes",
+// used by the new command for the <project-path> cli argument
+var moduleName *string
+
+var renames []rename = []rename{
+	{Src: "go.mod.tmpl", Dest: "go.mod"},
 }
 
-// newCmd represents the new command
 var newCmd = &cobra.Command{
 	Use:   "new <project-name>",
 	Short: "Create a new project with veggies",
@@ -39,8 +30,6 @@ var newCmd = &cobra.Command{
 	},
 	Args: cobra.MatchAll(cobra.ExactArgs(1)),
 }
-
-var moduleName *string
 
 func init() {
 	moduleName = newCmd.Flags().String("module-path", "veggie-app", "Module path for the project")
@@ -58,21 +47,6 @@ func writeFile(path, content string) error {
 	return err
 }
 
-func createDirs(name string) error {
-	if err := os.Mkdir(name, 0755); err != nil {
-		return fmt.Errorf("Error while creating main project directory: %w", err)
-	}
-
-	for _, subpath := range subdirs {
-		dir := name + subpath
-		if err := os.Mkdir(dir, 0755); err != nil {
-			return fmt.Errorf("Error while creating subdirectory '%s': %w", dir, err)
-		}
-	}
-
-	return nil
-}
-
 func runTidy(name string) (string, error) {
 	cmd := exec.Command("go", "mod", "tidy")
 	cmd.Dir = fmt.Sprintf("./%s", name)
@@ -83,27 +57,25 @@ func runTidy(name string) (string, error) {
 }
 
 // TODO:
-// add info output
 // maybe add options for db drivers
 func run(name string) error {
 	fmt.Println("Creating files and directories...")
-	if err := createDirs(name); err != nil {
-		return err
-	}
 
-	for _, entry := range mapping {
-		path := name + entry.Path
-		content := strings.ReplaceAll(entry.Content, "{{module}}", strings.TrimRight(*moduleName, "/"))
-		if err := writeFile(path, content); err != nil {
-			return fmt.Errorf("Error while writing template file '%s': %w", path, err)
-		}
-	}
-
-	fmt.Println("Running `go mod tidy`...")
-	out, err := runTidy(name)
-	fmt.Println(out)
+	subfs, err := fs.Sub(templates.Templates, "data")
 	if err != nil {
-		return fmt.Errorf("Error while running `go mod tidy`: %w", err)
+		return fmt.Errorf("Error while getting template subdir: %w", err)
+	}
+
+	if err := os.CopyFS(name, subfs); err != nil {
+		return fmt.Errorf("Error while copying templates: %w", err)
+	}
+
+	for _, r := range renames {
+		src := name + "/" + r.Src
+		dest := name + "/" + r.Dest
+		if err := os.Rename(src, dest); err != nil {
+			return fmt.Errorf("Error while renaming '%s' to '%s': %w", src, dest, err)
+		}
 	}
 
 	fmt.Printf("Successfully created new project '%s'\n", name)
--- internal/templates/templates.go
+++ internal/templates/templates.go
@@ -1,27 +1,6 @@
 package templates
 
-import _ "embed"
+import e "embed"
 
-//go:embed data/env.example
-var EnvExample string
-
-//go:embed data/main.go
-var Main string
-
-//go:embed data/go.mod.tmpl
-var Gomod string
-
-//go:embed data/query.sql
-var Query string
-
-//go:embed data/schema.sql
-var Schema string
-
-//go:embed data/sqlc.yaml
-var Sqlc string
-
-//go:embed data/handlers/greet.go
-var Greet string
-
-//go:embed data/routes/routes.go
-var Routes string
+//go:embed all:data
+var Templates e.FS
