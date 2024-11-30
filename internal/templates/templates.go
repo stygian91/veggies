54f9ba38251fe6839a73d8ede3280cc9e8835653
Styg <kioravuser@gmail.com>
2024-11-30 13:07:31 +0200

    Move cli commands to a separate package.

--- internal/templates/templates.go
+++
@@ -1,6 +0,0 @@
-package templates
-
-import e "embed"
-
-//go:embed all:data
-var Templates e.FS
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/stygian91/veggies/cmd"
-
-func main() {
-	cmd.Execute()
-}
